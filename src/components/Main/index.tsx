d693cfcc1e65aae39cb76e02dd47804d26021e5e
Danilo Mourelle <danilo.mourelle@icarotech.com>
2024-09-23 17:11:11 -0300

    refactor(main):update example to use props

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,9 +1,26 @@
 import * as S from "./styles";
 
-const Main = () => {
+interface MainProps {
+  title: string;
+  description: string;
+}
+
+const Main = ({
+  title = "React Avançado",
+  description = "Typescript, ReactJS, NextJS e Styled Components",
+}: MainProps) => {
   return (
     <S.Wrapper>
-      <h1>React Avançado</h1>
+      <S.Logo
+        src="/img/logo.svg"
+        alt="Imagem de um átomo e React avançado ao lado"
+      />
+      <S.Title>{title}</S.Title>
+      <S.Description>{description}</S.Description>
+      <S.Illustration
+        src="/img/hero-illustration.svg"
+        alt="Um desenvolvedor de frente para uma tela"
+      />
     </S.Wrapper>
   );
 };
