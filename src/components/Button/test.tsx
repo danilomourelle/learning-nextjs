73df864bb180a74b81aaaceaad07a342a158bfda
Danilo Mourelle <danilomourelle@outlook.com>
2024-09-27 16:08:33 -0300

    refactor(Button):add icon props to component

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -2,14 +2,16 @@ import { screen } from "@testing-library/react";
 
 import Button from ".";
 import { renderWithTheme } from "@/utils/tests/helpers";
+import { AddShoppingCart } from "@styled-icons/material-outlined";
 
 describe("<Button />", () => {
   it("should render the medium size by default", () => {
-    renderWithTheme(<Button>Lorem ipsum</Button>);
+    const { container } = renderWithTheme(<Button>Lorem ipsum</Button>);
 
-    const component = screen.getByRole("button", { name: /lorem ipsum/i });
+    const button = screen.getByRole("button", { name: /lorem ipsum/i });
 
-    expect(component).toHaveStyle({
+    expect(container.firstChild).toMatchSnapshot();
+    expect(button).toHaveStyle({
       height: "4rem",
       "font-size": "1.4rem",
       padding: "0.8rem 3.2rem 0.8rem 3.2rem",
@@ -18,9 +20,9 @@ describe("<Button />", () => {
   it("should render the small size when props set", () => {
     renderWithTheme(<Button size="small">Lorem ipsum</Button>);
 
-    const component = screen.getByRole("button", { name: /lorem ipsum/i });
+    const button = screen.getByRole("button", { name: /lorem ipsum/i });
 
-    expect(component).toHaveStyle({
+    expect(button).toHaveStyle({
       height: "3rem",
       "font-size": "1.2rem",
     });
@@ -28,9 +30,9 @@ describe("<Button />", () => {
   it("should render the large size when props set", () => {
     renderWithTheme(<Button size="large">Lorem ipsum</Button>);
 
-    const component = screen.getByRole("button", { name: /lorem ipsum/i });
+    const button = screen.getByRole("button", { name: /lorem ipsum/i });
 
-    expect(component).toHaveStyle({
+    expect(button).toHaveStyle({
       height: "5rem",
       "font-size": "1.6rem",
       padding: "0.8rem 4.8rem 0.8rem 4.8rem",
@@ -39,10 +41,21 @@ describe("<Button />", () => {
   it("should render the full width when props set", () => {
     renderWithTheme(<Button $fullWidth>Lorem ipsum</Button>);
 
-    const component = screen.getByRole("button", { name: /lorem ipsum/i });
+    const button = screen.getByRole("button", { name: /lorem ipsum/i });
 
-    expect(component).toHaveStyle({
+    expect(button).toHaveStyle({
       width: "100%",
     });
+  });
+  it("should render an icon when props set", () => {
+    renderWithTheme(
+      <Button icon={<AddShoppingCart data-testid="icon" />}>Buy now</Button>,
+    );
+
+    const button = screen.getByText(/buy now/i);
+    const icon = screen.getByTestId("icon");
+
+    expect(button).toBeInTheDocument();
+    expect(icon).toBeInTheDocument();
   });
 });
