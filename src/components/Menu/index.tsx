ea695f5142730e0525c298df30ad2ec29f032fca
Danilo Mourelle <danilomourelle@outlook.com>
2024-10-01 09:18:08 -0300

    refactor(Menu):implements changes for desktop version

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -7,6 +7,7 @@ import { ShoppingCart as ShoppingCartIcon } from "@styled-icons/material-outline
 import Logo from "@/components/Logo";
 import * as S from "./styles";
 import Button from "../Button";
+import MediaMatch from "../MediaMatch";
 
 type MenuProps = {
   username?: string;
@@ -17,12 +18,22 @@ export default function Menu({ username }: MenuProps) {
 
   return (
     <S.Wrapper>
-      <S.IconWrapper onClick={() => setIsOpen(true)}>
-        <MenuIcon aria-label="Open Menu" />
-      </S.IconWrapper>
+      <MediaMatch $lessThan="medium">
+        <S.IconWrapper onClick={() => setIsOpen(true)}>
+          <MenuIcon aria-label="Open Menu" />
+        </S.IconWrapper>
+      </MediaMatch>
       <S.LogoWrapper>
         <Logo $hideOnMobile />
       </S.LogoWrapper>
+
+      <MediaMatch $greaterThan="medium">
+        <S.MenuNav>
+          <S.MenuLink href="#">Home</S.MenuLink>
+          <S.MenuLink href="#">Explore</S.MenuLink>
+        </S.MenuNav>
+      </MediaMatch>
+
       <S.MenuGroup>
         <S.IconWrapper>
           <SearchIcon aria-label="Search" />
@@ -30,6 +41,11 @@ export default function Menu({ username }: MenuProps) {
         <S.IconWrapper>
           <ShoppingCartIcon aria-label="Open Shopping Cart" />
         </S.IconWrapper>
+        {!username && (
+          <MediaMatch $greaterThan="medium">
+            <Button>Sign-in</Button>
+          </MediaMatch>
+        )}
       </S.MenuGroup>
 
       <S.MenuFull aria-hidden={!isOpen} $isOpen={isOpen}>
